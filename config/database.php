d31a9e674795f538efb219b5ede6de954d3050ae
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-06-23 20:33:42 -0600

    v0.2.0 Se integran fotos de hogar iris

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+namespace config;
+
+class database{
+    public string $db_name = 'inmobiliaria'; //Name database
+    public string $db_user = 'root'; // User database
+    public string $db_password = 'Karate001.'; // Pass database
+    public string $db_host = 'localhost'; // Host Database
+    public string $set_name = 'utf8'; // Host Database
+    public string $time_out = '10'; // Host Database
+    public string $sql_mode = ''; // Host Database
+}
--- config/generales.php
+++ config/generales.php
@@ -1,24 +1,15 @@
 <?php
 namespace config;
 class generales{
-    public bool $muestra_index = true;
     public string $path_base;
     public string $session_id = '';
-        public string $sistema = 'web_tique';
     public string $url_base = 'http://localhost/web_tique/';
-    public array $secciones = array();
-    public bool $encripta_md5 = false;
-    public bool $aplica_seguridad = true;
-    public array $defaults ;
-
     public function __construct(){
-        //$this->path_base = getcwd();
         $this->path_base = '/var/www/html/web_tique/';
+
+        $this->muestra_index = true;
         if(isset($_GET['session_id'])){
             $this->session_id = $_GET['session_id'];
         }
-        $this->defaults['dp_pais']['id'] = 121;
-        $this->defaults['dp_estado']['id'] = 1;
-        $this->defaults['dp_municipio']['id'] = 1;
     }
 }
