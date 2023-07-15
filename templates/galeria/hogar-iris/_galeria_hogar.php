af3e40e56113a663e499348662df3949e56000ca
samuelalexsanche <saul.garcia@ivitec.com.mx>
2023-07-14 19:07:29 -0600

    v0.15.5 se corrige la galeria de hogar iris

--- templates/galeria/hogar-iris/_galeria_hogar.php
+++ templates/galeria/hogar-iris/_galeria_hogar.php
@@ -1,6 +1,6 @@
 <?php
 $a = $generales->url_base."assets/img/venta_casas/hogar_iris/1Fachada/img1.jpg";
-$b = $generales->url_base."assets/img/venta_casas/hogar_iris/3Cocina/Cocina_3.jpg";
+$b = $generales->url_base."assets/img/venta_casas/hogar_iris/3Cocina/cocina_3.jpg";
 $c = $generales->url_base."assets/img/venta_casas/hogar_iris/5Baño/Baño_1.jpg";
 ?>
 
