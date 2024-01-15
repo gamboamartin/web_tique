a63d594c5eddccf3695c03e6b0f1df90a2911666
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-01-15 10:34:52 -0600

    v0.41.10 se integran hogares a elige tu hogar

--- templates/lista_casas/elige-tu-hogar/_lista_casas_elije_tu_hogar.php
+++ templates/lista_casas/elige-tu-hogar/_lista_casas_elije_tu_hogar.php
@@ -4,14 +4,19 @@ $generales = new generales();
 ?>
 <div class="properties">
     <div class="row">
-        <?php include $generales->path_base.'templates/lista_casas/hogar-xiadani/_carta_casa.php' ?>
-        <?php include $generales->path_base.'templates/lista_casas/hogar-izel/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-colibri/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-glicinia/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-margarita/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-ambar/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-ladera/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/lote-itze/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-casuarina/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-iris/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-pasitea/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-araucaria/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/hogar-paraiso/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/hogar-orquidea/_carta_casa.php' ?>
-        <?php include $generales->path_base.'templates/lista_casas/hogar-haru/_carta_casa.php' ?>
-        <?php include $generales->path_base.'templates/lista_casas/hogar-pasitea/_carta_casa.php' ?>
-        <?php include $generales->path_base.'templates/lista_casas/hogar-iris/_carta_casa.php' ?>
-        <?php include $generales->path_base.'templates/lista_casas/lote-itze/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-izel/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/hogar-eirene/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/lote-yaax/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/hogar-venus/_carta_casa.php' ?>
@@ -24,6 +29,8 @@ $generales = new generales();
         <?php include $generales->path_base.'templates/lista_casas/hogar-teva/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/lote-nicte/_carta_casa.php' ?>
         <?php include $generales->path_base.'templates/lista_casas/hogar-xadani/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-haru/_carta_casa.php' ?>
+        <?php include $generales->path_base.'templates/lista_casas/hogar-xiadani/_carta_casa.php' ?>
 
     </div><!-- /.properties -->
 </div> <!-- /.properties-->
