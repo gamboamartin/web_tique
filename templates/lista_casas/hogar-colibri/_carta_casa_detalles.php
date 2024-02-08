819051fa72ff6ee9907a7a72244204bb4bac4396
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-02-08 15:21:52 -0600

    v0.43.13 se corrige direccion de hogar colibri

--- templates/lista_casas/hogar-colibri/_carta_casa_detalles.php
+++ templates/lista_casas/hogar-colibri/_carta_casa_detalles.php
@@ -10,8 +10,8 @@ if(count($a)>2) {
 }
 ?>
 <div class="property-card-box card-box card-block" style="height: 170px !important;">
-    <h3 class="property-card-title"><a href="<?php echo $generales->url_base; ?>hogares/hogar-glicinia.php">Hogar Colibri</a></h3>
-    <div class="property-card-descr">C. Dr Fernando Banda #108 Int. 104, Col. Constitucion, Zapopan Jal.</div>
+    <h3 class="property-card-title"><a href="<?php echo $generales->url_base; ?>hogares/hogar-colibri.php">Hogar Colibri</a></h3>
+    <div class="property-card-descr">C. Dr Fernando Banda #108 Int. 104, Col. Los Laureles, Zapopan Jal.</div>
     <div class="property-preview-footer  clearfix">
         <div class="property-preview-f-left text-color-primary">
             <span class="property-card-value">
